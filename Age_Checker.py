5b20bd097ebefbaaf91b8ffca785fa6c0d0f8df1
REpookit145 <Sebi.wolter3012@web.de>
2026-09-13 22:56:50 +0000

    Add files via upload

---
+++ Age_Checker.py
@@ -0,0 +1,34 @@
+import subprocess
+import sys
+ 
+# Installiert pyfiglet automatisch, falls es noch nicht da ist
+try:
+    import pyfiglet
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyfiglet"])
+    import pyfiglet
+ 
+def rainbow_text(text):
+    # ANSI 256-color codes that go through the rainbow
+    colors = [196, 202, 208, 220, 226, 118, 46, 51, 21, 93, 165]
+    colored = ""
+    for i, char in enumerate(text):
+        color = colors[i % len(colors)]
+        colored += f"\033[38;5;{color}m{char}"
+    colored += "\033[0m"  # Farbe am Ende zuruecksetzen
+    return colored
+ 
+# Cooler ASCII-Art Titel
+banner = pyfiglet.figlet_format("AGE CHECK")
+for line in banner.split("\n"):
+    print(rainbow_text(line))
+ 
+age = int(input("Please enter your age: "))
+ 
+if age < 10:
+    print("Kiddo")
+elif age < 18:
+    print("Underage")
+else:
+    print("Adult")
+    
